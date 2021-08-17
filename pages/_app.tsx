1150c6d25842b390beaa740badb3cbe12f93a768
daichi-sato-design <daichisato.design@gmail.com>
2021-08-17 22:43:13 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { VFC } from 'react'
+
+const MyApp: VFC = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
