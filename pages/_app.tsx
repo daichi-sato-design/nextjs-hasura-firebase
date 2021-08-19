a06b1beece789947677a7e749a5f8dd6ce0096eb
daichi-sato-design <daichisato.design@gmail.com>
2021-08-19 20:15:21 +0900

    ProviderでComponentをラップ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import { VFC } from 'react'
 import { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 
+import { store } from '../app/store'
 import { useUserChanged } from '../hooks/useUserChanged'
 
 const MyApp: VFC = ({ Component, pageProps }: AppProps) => {
   const {} = useUserChanged()
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
