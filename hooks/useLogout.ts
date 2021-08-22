daf2977b27897fb56be3e1fa39600664f2023ab9
daichi-sato-design <daichisato.design@gmail.com>
2021-08-22 22:21:45 +0900

    Logout時にCacheとReduxのリセットを追加

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,24 @@
 import Cookie from 'universal-cookie'
 import firebase from '../firebaseConfig'
 import { unSubMeta } from './useUserChanged'
+import { useQueryClient } from 'react-query'
+import { useDispatch } from 'react-redux'
+import { resetEditedTask, resetEditedNews } from '../slices/uiSlice'
 
 const cookie = new Cookie()
 
 export const useLogout = () => {
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
   const logout = async () => {
     if (unSubMeta) {
       unSubMeta()
     }
     await firebase.auth().signOut()
+    dispatch(resetEditedTask())
+    dispatch(resetEditedNews())
+    queryClient.removeQueries('tasks')
+    queryClient.removeQueries('news')
     cookie.remove('token')
   }
 
