0ee3afdaceec3523f7cb3740978b22138b606aff
daichi-sato-design <daichisato.design@gmail.com>
2021-08-23 21:30:31 +0900

    indexページに表示するnewsをgetStaticPropsを使用しSSGで実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
+import { GetStaticProps } from 'next'
 import { VFC } from 'react'
-
 import { Auth } from '../components/Auth'
 import { Layout } from '../components/Layout'
+import { dehydrate } from 'react-query/hydration'
+import { fetchNews } from '../hooks/useQueryNews'
+import { News } from '../types/types'
+import { QueryClient, useQueryClient } from 'react-query'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery('news', fetchNews)
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 3,
+  }
+}
 
 const Home: VFC = () => {
+  const queryClient = useQueryClient()
+  const data = queryClient.getQueryData<News[]>('news')
   return (
     <Layout title="Home">
+      <p className="mb-5 text-blue-500 text-xl">News list by SSG</p>
+      {data?.map((news) => (
+        <p className="font-bold" key={news.id}>
+          {news.content}
+        </p>
+      ))}
       <Auth />
     </Layout>
   )
